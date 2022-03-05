fb4debbef8873ce66166bbaa807afa65bfc1390a
fatihGunal <fatihgunal2018@gmail.com>
2022-03-05 03:29:24 +0100

    creating Recommendations service: Temprory suspended

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spotify-clone-java
 Spotify clone
+
+
+Recommendations -> temporary suspended, nice to have in future
+Profiles service -> postgresql
+Songs -> MongoDB
+Playlist -> postgresql
---
+++ Recommendations/src/main/java/com/spotifyclone/recommendations/RecommendationsApplication.java
@@ -0,0 +1,11 @@
+package com.spotifyclone.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationsApplication.class, args);
+    }
+}
