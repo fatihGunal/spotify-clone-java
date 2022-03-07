ff9195b70fa6c1f54259b4c77a603b775e398774
fatihGunal <fatihgunal2018@gmail.com>
2022-03-08 00:34:07 +0100

    adding config for eureka

--- eureka-server/src/main/java/com/spotifyclone/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/spotifyclone/eurekaserver/EurekaServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+    // replace eureka in the future with kubernetes
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
