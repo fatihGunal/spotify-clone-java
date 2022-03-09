29cfee91ef2c2aca5f295aa3fdb5cd6f31eaf735
fatihGunal <fatihgunal2018@gmail.com>
2022-03-10 00:16:57 +0100

    ending discovery service, webclient instance via bean and loadbalancing

---
+++ music/src/main/java/com/spotifyclone/music/config/WebFluxConfig.java
@@ -0,0 +1,18 @@
+package com.spotifyclone.music.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder getWebClient() {
+        return WebClient.builder();
+    }
+}
--- music/src/main/java/com/spotifyclone/music/profiles/ProfileService.java
+++ music/src/main/java/com/spotifyclone/music/profiles/ProfileService.java
@@ -1,13 +1,22 @@
 package com.spotifyclone.music.profiles;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @Slf4j
-public record ProfileService(ProfileRepository profileRepository) {
+public class ProfileService {
 
+    private final ProfileRepository profileRepository;
+    private final WebClient.Builder webClient;
+
+    @Autowired
+    public ProfileService(ProfileRepository profileRepository, WebClient.Builder webClient) {
+        this.profileRepository = profileRepository;
+        this.webClient = webClient;
+    }
 
     public void registerProfile(final ProfileRegistrationResponse request) {
         Profile profile = Profile.builder()
@@ -23,13 +32,13 @@ public record ProfileService(ProfileRepository profileRepository) {
         log.info("Do something with fraudResponse {}", fraudResponse);
     }
 
-    @LoadBalanced
     private FraudResponse askFraud(final Profile profile) {
 
         // FRAUD as baseUrl is because we use Eureka service discovery, kinda cool!
-        WebClient webClient = WebClient.builder().baseUrl("http://FRAUD:8081").build();
-
+        // Later replacing Eureka with kubertes
         FraudResponse fraudResponse = webClient
+                .baseUrl("http://FRAUD")
+                .build()
                 .get()
                 .uri("/api/v1/fraud-check/{profileId}", profile.getId())
                 .retrieve()
